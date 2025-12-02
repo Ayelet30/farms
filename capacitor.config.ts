63b5c409196b4d6b74564b5518a1e5b58eebe2bf
ayelet30 <ayelethury@gmail,com>
2025-12-02 18:09:16 +0200

    הוסםת האפשרות לאפליקציה

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'org.smartfarm.app',
+  appName: 'smart-farm',
+  webDir: 'dist'
+};
+
+export default config;
