8864d2a450dbe8922e14a955c499901896277ca2
ayelet30 <ayelethury@gmail,com>
2026-04-26 12:52:42 +0300

    שיפור נראות כניסה למערכת

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { MatDialog } from '@angular/material/dialog';
 import { LoginComponent } from '../login/login.component';
@@ -12,26 +11,12 @@ import { LoginComponent } from '../login/login.component';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-  constructor(private router: Router,
-    private dialog: MatDialog
-  ) {}
+  constructor(private dialog: MatDialog) {}
 
   goToLogin() {
     this.dialog.open(LoginComponent, {
       width: '350px',
       panelClass: 'custom-login-dialog'
     });
-  }
-
-  openBooking(type: string) {
-    if (type == 'therapeutic')
-    {
-        this.goToLogin();
-        return;
-    }
-    else{
-      this.router.navigate(['/booking', type]);
-    }
-    
   }
 }
